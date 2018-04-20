90531989d724009e160085aeee3aff54659caa5b
linyongzhi <yongzhi.lin@uama.com.cn>
2018-04-20 14:32:25 +0800

    清理不再使用的 ribbon相关代码

--- src/main/java/com/babytiger/springboot/microservice/forex/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/babytiger/springboot/microservice/forex/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
@@ -1,13 +1,11 @@
 package com.babytiger.springboot.microservice.forex.springbootmicroservicecurrencyconversion;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @FeignClient(name="forex-service")
-@RibbonClient(name = "forex-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue
